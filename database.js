bbde9f6e85f83fccb6057eaac96c31798103949a
Ippolito Daphne <g1daphne@cdf.toronto.edu>
2013-02-21 12:47:28 -0500

    filled in the two database insert methods

--- database.js
+++ database.js
@@ -24,16 +24,41 @@ function connect() {
     connection.connect();
 }
 
-function insertLikedPost(username, url, image, text, note_count) {
-
+/** Inserts a post liked by one of the tracked blocks into the database.
+*
+**/
+function insertLikedPost(url, username, image, text, note_count) {
+	connection.query('INSERT INTO liked_posts VALUES(' +
+						url + ", " +
+						username + ", " +
+						'CURTIME(), ' + 
+						image + ", " +
+						text + ", " +
+						note_count + ", " +
+						'0);',
+			function(err, rows, fields) {
+    			if (err) throw err;
+   				else console.log('Inserted new post liked by ' +
+   									username + " with url: " + url);
+			});
 }
 
+/** Inserts a new blog to be tracked
+*
+**/
 function insertNewBlog(url, username) {
-
+	connection.query('INSERT INTO tracked_blogs VALUES(' +
+						url + ", " +
+						username + ");", 
+			function(err, rows, fields) {
+    			if (err) throw err;
+   				else console.log('Inserted new block to track, authored by ' +
+   									username + " with url: " + url);
+			});
 }
 
 function updatePostPopularity(url, increment) {
-
+		
 }
 
 /*returns the popularity of the input post*/
