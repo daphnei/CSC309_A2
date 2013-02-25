ca28e2136fa742ed0b880f7ad073fbc7fc25c54a
akbiggs <alex.biggs@utoronto.ca>
2013-02-24 23:43:03 -0500

    fixed schema -Daphne

--- sql/tables.sql
+++ sql/tables.sql
@@ -15,18 +15,12 @@ CREATE TABLE liked_posts(
     KEY(url)
 );
 
-CREATE TABLE likes(
-    blog VARCHAR(200),
-    post VARCHAR(200),
-    KEY(blog, post)
-);
-
 CREATE TABLE updates(
     url VARCHAR(200),
     time DATETIME,
     sequence_index INTEGER,
     increment INTEGER,
-    KEY(url, sequence)
+    KEY(url, sequence_index)
 );
 
 CREATE TABLE unicorns(
