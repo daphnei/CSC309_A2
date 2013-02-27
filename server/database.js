115e777076cb27b3a86b6fe2f7d9f370b9530c09
Ippolito Daphne <g1daphne@cdf.toronto.edu>
2013-02-27 17:48:21 -0500

    A lot of changes to the database code architecture. Now, all queries to the database are being added to a queue, and executed one a time. Might have a problem with this scaling, if we get large amounts of queries to do in a very short period of time. There are still bugs in this version. Will sort them out and commit again soon.

--- server/database.js
+++ server/database.js
@@ -1,10 +1,20 @@
 /* database.js
  * Methods for accessing database information for this assignment.
  */
-
+ 
+//will use this to prevent queries from overlapping
 var mysql = require("mysql");
+var queue =  require("./queue.js");
 
+/**
+ * A constant representing the quote string
+ **/
 var QUOTE = "'";
+
+/**
+ * A list of all of the table names. This will be iterated over to clear them. 
+ */
+var tables = new Array('updates', 'tracked_blogs','liked_posts');
 
 // edit as necessary
 var HOST = "dbsrv1.cdf.toronto.edu",
@@ -22,6 +32,8 @@ var options = {
 		password: PWD
 	};
 
+var queryQ = new queue.Queue();
+var queriesExecuting = false;
 /**
  * Connect to the database.
  *
@@ -50,29 +62,32 @@ function disconnect(connection) {
  * @param text The text of the post. Null if post has no text.
  * @param note_count How many notes have been made so far on the post.
  * 
- * @returns true upon success, false otherwise
  */
 function insertLikedPost(url, username, image, text, note_count) {
+	//This connection is only used so that escape can be called to check 
+	//against sql injection. The actual querying of the database is done
+	//by processQueryQueue
 	var connection = connect();
 	if (!connection) 
-		return false;
+		throw "Could not connect to the database."
 		
 	var queryText = "INSERT INTO liked_posts VALUES(" +
-						QUOTE + url + QUOTE + ", " +
-						QUOTE + username + QUOTE + ", " +
+						connection.escape(url) + ", " +
+						connection.escape(username) + ", " +
 						'CURTIME(), ' + 
-						QUOTE + image + QUOTE + ", " +
-						QUOTE + text + QUOTE + ", " +
+						connection.escape(image) + ", " +
+						connection.escape(text) + ", " +
 						note_count + ", " +
 						'0);';
-	connection.query(queryText,
-					function(err, rows, fields) {
-						if (err) throw err;
-		   				else console.log('Inserted new post liked by ' +
-		   									username + " with url: " + url);
-					});
 
 	disconnect(connection);
+	var item = new queue.Item(queryText, null, null);
+	queryQ.enqueue(item);
+	
+	//if not already in the process of executing all queries in the 
+	//queue, then start doing so
+	if(!queriesExecuting) processQueryQueue();
+	
 	return true;
 }
 
@@ -82,75 +97,118 @@ function insertLikedPost(url, username, image, text, note_count) {
  * @param url The url of the blog.
  * @param username The username of the owner of the blog.
  * 
- * @returns true upon success, false otherwise
  */
 function insertNewBlog(url, username) {
+	//This connect is only used so that escape can be called to check 
+	//against sql injection. The actual querying of the datavase is done
+	//by processQueryQueue
 	var connection = connect();
-	if (!connection)
-		return false;
+	if (!connection) 
+		return false;	
 	var queryText = "INSERT INTO tracked_blogs VALUES(" +
-						QUOTE + url + QUOTE + "," +
-						QUOTE + username + QUOTE + ");";
-	console.log(queryText);
-	connection.query(queryText, 
-					function(err, rows, fields) {
-						if (err) throw err;
-		   				else console.log('Inserted new block to track, ' +
-							'authored by ' + username + " with url: " + url);
-					});
-
+						connection.escape(url)  + "," +
+						connection.escape(username) + ");";
 	disconnect(connection);
-	return true;
+	var item = new queue.Item(queryText, null, null);
+	queryQ.enqueue(item);
+	
+	//if not already in the process of executing all queries in the 
+	//queue, then start doing so
+	if(!queriesExecuting) processQueryQueue();
 }
 
 /**
  * Makes an update to the popularity of a post.
  *
+ * There are three steps:
+ * 1) Find the number of updates that have been done so far on a given 
+ *    liked post.
+ * 2) Increment the value for the number of updates that have been done 
+ *    on that post.
+ * 3) Add a new tuple to updates with this information.
+ * 
  * @param url The url of the post.
  * @param increment How much the post's popularity(note count) has increased
  *				  since the last update.
  * 
- * @returns true upon succes, false if there was a failure
  */
 function updatePostPopularity(url, increment) {
 	var connection = connect();
 	if (!connection) 
-		return false;
+		throw "Could not connect to the database."
 		
-	// first get the number of updates that have been done for this url, 
-	// so that we know what sequence index to use
+	//find the total number of updates made so for this liked post
 	var queryText = 'SELECT num_updates FROM liked_posts WHERE url == ' + 
-		QUOTE + url + QUOTE + ';';
-	var index;
-	connection.query(queryText,
-					function(err, rows, fields) {
-						if (err) throw err;
-						// +1 because this method is making a new update
-						else index = rows[0].num_updates + 1; 
-					});
-
-	// now, increment num_updates, because this method is doing a new update
-	queryText = 'UPDATE liked_posts SET num_updates=' + index + 
-		'WHERE url ==' + QUOTE + url + QUOTE + ';';
-	connection.query(queryText,
-					function(err, rows, felds) {
-						if (err) throw err;
-					});
+		QUOTE + connection.escape(url) + QUOTE + ';';
 	
+	disconnect(connection);
+	
+	//when the query for this item has finished, incrementNumUpdates will
+	//be called with [url, increment] as its parameter.
+	var item = new queue.Item(queryText, incrementNumUpdates, [url, increment]);
+	queryQ.enqueue(item);
+	
+	if(!queriesExecuting) processQueryQueue();
+}
+
+/**  PRIVATE
+ * A helper function for updatePostPopularity that increments num_updates
+ * for a liked post. This function is not called directly, but rather 
+ * called from a callback upon the successful completion of a query.
+ * 
+ * @param(params) a list conststing of [url, increment]
+ * @params(rows) the rows returned by the query that this method was called for
+ **/
+ function incrementNumUpdates(params, rows) {
+	var url = params[0];
+	var increment = params[1];
+	
+	var connection = connect();
+	if (!connection) 
+		throw "Could not connect to the database."
+		
+	// +1 from olde value, because we are in the process of making a new
+	// update.
+	var new_num = rows[0].num_updates + 1; 
+	
+	//next update the value of the total number of updates to reflect the
+	//current update being done
+	var queryText = 'UPDATE liked_posts SET num_updates=' + new_num + 
+		'WHERE url ==' + connection.escape(url) + ';';
+	disconnect(connection);
+		
+	var item = new queue.Item(queryText, insertUpdateTuple,
+								[url, increment, new_num]);
+	queryQ.enqueue(item);
+	if(!queriesExecuting) processQueryQueue();
+ }
+ 
+/**  PRIVATE
+ * A helper function for updatePostPopularity that creates a new updates
+ * tuple with the new increment for a liked post. This function is not 
+ * called directly, but rather called from a callback upon the successful
+ * completion of a query.
+ * @param(params) a list conststing of [url, increment, update_index]
+ **/
+function insertUpdateTuple(params) {	
+	var url = params[0];
+	var increment = params[1];
+	var index = params[2];
+	
+	var connection = connect();
+	if (!connection) 
+		throw "Could not connect to the database."
+		
 	queryText = "INSERT INTO updates VALUES(" +
-					QUOTE + url + QUOTE + ", " +
+					connection.escape(url) + ", " +
 					"CURTIME(), " + 
 					index + ", "  + 
 					increment + ");";
-	connection.query(queryText,
-					function(err, rows, fields) {
-						if (err) throw err;
-		   				else console.log('Successfully did update ' + 
-							index + 'to url ' + url);
-					});	
-
 	disconnect(connection);
-	return true;
+	
+	var item = new queue.Item(queryText, null, null); //thank god, no more functions to call
+	queryQ.enqueue(item);
+	if(!queriesExecuting) processQueryQueue();
 }
 
 /**
@@ -196,26 +254,75 @@ function getRecentPosts() {
  * clears all entries in all the table. Mainly used for testing, in reality
  * we should not have to every call this.
  * 
- * @returns trur upon success, false for failure
  */
-function clearDatabase() {
-	var connection = connect();
-	if (!connection)
-		return false;
-	var query = 'TRUNCATE TABLE tracked_blogs; ' + 
-				'TRUNCATE TABLE liked_posts; ' +
-				'TRUNCATE TABLE likes; ' +
-				'TRUNCATE TABLE updates; ';
-	
-	connection.query(queryText,
-					function(err, rows, fields) {
-						if (err) throw err;
-		   				else console.log('Successfully cleared the database.');
-					});
-	disconnect(connection);
-	return true;
+function clearTables() {
+	for (var i = 0; i < tables.length; i++) {
+		var item = new queue.Item("TRUNCATE TABLE " + tables[i] + ";", null, null);
+		queryQ.enqueue(item);
+	}
+
+	//if not already in the process of executiving all queries in the 
+	//queue, then start doing so
+	if(!queriesExecuting) processQueryQueue();
 }
 
+
+/**
+ * A method that recursively goes through all queries in the queue and
+ * executes them in order, by only starting execution of the next one
+ * once the current one is finished executing.
+ **/
+function processQueryQueue() {	
+	queriesExecuting = true;
+	
+	var connection = connect();
+	if (!connection)
+		throw "Could not connect to the database."
+		
+	if (queryQ.getLength() > 0) {
+		var currentQueryItem = queryQ.dequeue();
+		
+		//need to do this in a seperate function for namespace issues
+		//if we don't do this, it will be impossible to know what query
+		//has just been executed when the callback function is reached
+		sendQuery(connection, currentQueryItem);
+	} else {
+		//no more queries to execute for now, so no more levels of recursion
+		//for now
+		queriesExecuting = false;
+	}
+
+	disconnect(connection);
+}
+
+/**
+ * A helper method for processQueryQueue. This is needed so that we can
+ * print out what query just finished executing, once it is done.
+ * 
+ * @param(connection) the connection to the database
+ * @param(queryString) the query to be executed
+ **/
+function sendQuery(connection, item) {
+	connection.query(item.queryString, 
+			function(err, rows, fields) {
+				if (err) {
+					if(err.code == 'ER_DUP_ENTRY') 
+						console.log("WARNING: The query '" + item.queryString +
+									"' was not executed because it violates a primary key constraint.");
+				}
+				else console.log("Just executed: " + item.queryString);
+				
+				//do whatever action was passed in
+				if (item.action != null) {
+					item.action(item.actionParams, rows, fields);
+				}
+				
+				//process the next query in the queue
+				processQueryQueue();
+			});
+}
+
+exports.clearTables = clearTables;
 exports.insertLikedPost = insertLikedPost;
 exports.insertNewBlog = insertNewBlog;
 exports.updatePostPopularity = updatePostPopularity;
--- server/testdatabase.js
+++ server/testdatabase.js
@@ -1,3 +1,17 @@
 var database = require("./database.js");
+console.log("Working, working, nope!");
 
-database.insertNewBlog("Stupid", "face");
+database.clearTables();
+database.insertNewBlog("Stupid.org", "face");
+database.insertNewBlog("Happy.com", "face");
+database.insertNewBlog("Smily.com", "face");
+database.insertNewBlog("Smily.com", "I am a duplicate");
+database.insertLikedPost("Hope.com", "face", "image", "text", 24);
+database.updatePostPopularity("Hope.com", 13);
+/*
+setTimeout(function() {
+	database.insertNewBlog("Stupid.org", "face");
+	database.insertNewBlog("Happy.com", "face");
+	}, 500); 
+*/
+
