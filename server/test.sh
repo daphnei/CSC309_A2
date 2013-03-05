3e84741a939c392ae23f7db889e9de1118a107de
Gabriel B. Nunes <alchemicvisions@gmail.com>
2013-03-05 00:44:52 -0500

    Added handler and tests for single-blog trends.

--- server/test.sh
+++ server/test.sh
@@ -20,3 +20,24 @@ curl -i -X POST "localhost:31285/blogs/trends?order=Trending" # Should return 40
 curl -i -X GET "localhost:31285/blogs/trends" # Should return 400
 curl -i -X GET "localhost:31285/blogs/trends?order=Potato" # Should return 400
 
+echo "
+---------- Testing retrieval of individual blogs' trends ----------
+" # Needs a database connection
+
+curl -i -X GET "localhost:31285/blog/mspandrew.tumblr.com/trends?order=Trending"
+# Should succeed
+curl -i -X GET "localhost:31285/blog/mspandrew.tumblr.com/trends?order=Recent"
+# Should succeed
+curl -i -X GET "localhost:31285/blog/mspandrew.tumblr.com/trends?order=Trending&limit=5"
+# Should succeed
+curl -i -X GET "localhost:31285/blog/mspandrew.tumblr.com/trends?order=Recent&limit=5"
+# Should succeed
+curl -i -X POST "localhost:31285/blog/mspandrew.tumblr.com/trends?order=Trending"
+# Should return 404
+curl -i -X GET "localhost:31285/blog/notch.tumblr.com/trends?order=Trending"
+# Should return 404, as it's a valid blog that we're not following.
+curl -i -X GET "localhost:31285/blog/notarealblogsahjflisadncuw.tumblr.com/trends?order=Trending"
+# Should return 404, as it's not a valid blog and we shouldn't be following it.
+curl -i -X GET "localhost:31285/blog/mspandrew.tumblr.com/trends" # Should return 400
+curl -i -X GET "localhost:31285/blog/mspandrew.tumblr.com/trends?order=Potato"
+# Should return 400
